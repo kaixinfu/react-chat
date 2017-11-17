7956b8166d5d503c81ad1d4e3b3369c713a02327
liukaixin01 <1360517661@qq.com>
2017-11-17 22:53:28 +0800

    fix README.md

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import { Button } from 'antd-mobile';
+import { Button , List} from 'antd-mobile';
 import {createStore} from 'redux';
 import logo from './logo.svg';
 import './App.css';
+const Item = List.Item;
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            list: ['苍井空', '饭岛爱', '麻生希']
+        }
+    }
+	addItem() {
+        this.setState({
+			list: [...this.state.list, '毛衣妹' + Math.random()]
+        })
+    }
   render() {
     return (
       <div className="App">
@@ -15,7 +27,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <Button type="primary">按钮</Button>
+          <Button onClick={() => this.addItem()} type="primary">添加女优</Button>
+          <List renderHeader={() => '女忧列表'}>
+              {
+                  this.state.list.map((_, key) => <Item key={key} onClick={() => {}}>
+                      <Item>{_}</Item>
+                  </Item>)
+              }
+          </List>
       </div>
     );
   }
