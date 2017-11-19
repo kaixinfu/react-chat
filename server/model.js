cf2368904890924cbaf277711e1d17f7f3b2f5b6
liukaixin01 <1360517661@qq.com>
2017-11-19 20:11:55 +0800

    create mongoose Schema

--- server/model.js
+++ server/model.js
@@ -1,6 +1,32 @@
 const mongoose = require('mongoose');
-const DB_URL = 'mongodb://localhost:27017/chat';
+const DB_URL = 'mongodb://localhost:27017/react-chat';
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected', function () {
 	console.log('mongoose connect =======> succse')
 })
+
+const models = {
+	user: {
+		'user': {type: String, require: true},
+		'password': {type: String, require: true},
+		'type': {type: String, require: true},
+		'avatar': {type: String},
+		'desc': {type: String},
+		'title': {type: String},
+		'company': {type: String},
+		'money': {type: String},
+	},
+	chat: {
+	
+	}
+}
+
+for (let i in models) {
+	mongoose.model(i, new mongoose.Schema(models[i]))
+}
+
+module.exports = {
+	getModel: function(name) {
+		return mongoose.model(name)
+	}
+}
--- server/user.js
+++ server/user.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const Router = express.Router();
+const model = require('./model');
+const User = model.getModel('user');
 
 Router.get('/info', function (rep, res) {
 	return res.json({code: 1})
+});
+
+Router.get('/list', function (rep, res) {
+	User.find({}, function (error, doc) {
+		return res.json(doc)
+	})
 });
 
 module.exports = Router
