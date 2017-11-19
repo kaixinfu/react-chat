478c517f1eaf13307c31219cd42bc8925834b16f
liukaixin01 <1360517661@qq.com>
2017-11-19 12:38:57 +0800

    server undefined

--- server/user.js
+++ server/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Router = express.Router();
 
 Router.get('/info', function (rep, res) {
+	console.log('rep ========================================> ', rep)
+	console.log('res =========================================> ', res)
 	return res.json({code: 1})
 });
 
