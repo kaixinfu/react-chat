04e6a26f084d6bbc469cccf451bca5ca3bd62925
liukaixin01 <1360517661@qq.com>
2017-11-20 23:42:12 +0800

    set login cookie

--- server/user.js
+++ server/user.js
@@ -3,9 +3,23 @@ const utils = require('utility');
 const Router = express.Router();
 const model = require('./model');
 const User = model.getModel('user');
+const filter = {password: 0, __v: 0}
 
 Router.get('/info', function (req, res) {
-	return res.json({code: 1})
+	const {userid} = req.cookies
+	console.log('userid ========> ', userid)
+	if (!userid) {
+		return res.json({code: 1})
+	} else {
+		User.findOne({_id: userid}, filter, function (error, doc) {
+			if (error) {
+				return res.json({code: 1, message: '网路异常'})
+			}
+			if (doc) {
+				return res.json({code: 0, data: doc})
+			}
+		})
+	}
 });
 
 Router.get('/list', function (req, res) {
@@ -44,10 +58,11 @@ Router.post('/login', function (req, res) {
 		user,
 		password,
 	} = req.body
-	User.findOne({user, password: md5password(password)}, {password: 0, __v: 0, _id: 0}, function (error, doc) {
+	User.findOne({user, password: md5password(password)}, filter, function (error, doc) {
 		if (!doc) {
 			return res.json({code: 1, message: '用户名或者密码错误'})
 		} else {
+			res.cookie('userid', doc._id)
 			return res.json({code: 0, message: '登陆成功', data: doc})
 		}
 	})
